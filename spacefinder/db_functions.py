c0aad7a3155aa43bec0d59fbee67e61a2c6fd7d6
Joseph Turner <turner@miserware.com>
2013-09-13 09:58:15 -0400

    Implement datatables and style it for bootstrap

--- spacefinder/db_functions.py
+++ spacefinder/db_functions.py
@@ -4,8 +4,9 @@ from models import db, Listing, Account
 def init_db():
     db.drop_all()
     db.create_all()
-    db.session.add(Listing("1500 Union Ave #2500, Baltimore, MD",
-                           "39.334497", "-76.64081", "office", 2500,
-                           "Headquarters for Maryland Nonprofits"))
+    for i in range(100):
+        db.session.add(Listing("1500 Union Ave #2500, Baltimore, MD",
+                               "39.334497", "-76.64081", "office", 2500,
+                               "Headquarters for Maryland Nonprofits"))
     db.session.add(Account("admin", "example@example.com", "Pass1234"))
     db.session.commit()
