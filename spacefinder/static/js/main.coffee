9d2db18f936a063e52569f437384027df27c26a5
Joseph Turner <turner@miserware.com>
2013-09-21 12:30:17 -0400

    Handle the case when there are no listings.

--- spacefinder/static/js/main.coffee
+++ spacefinder/static/js/main.coffee
@@ -3,15 +3,19 @@ drawMap = (locations) ->
     mapTypeId: google.maps.MapTypeId.ROADMAP
   }
   map = new google.maps.Map(document.getElementById('listing-map'), mapOptions)
-  bounds = new google.maps.LatLngBounds()
-  for location in locations
-    loc = new google.maps.LatLng(location[0], location[1])
-    marker = new google.maps.Marker({
-        map: map,
-        position: loc,
-    })
-    bounds.extend(loc)
-  map.setCenter(bounds.getCenter())
-  map.fitBounds(bounds)
+  if locations.length > 0
+    bounds = new google.maps.LatLngBounds()
+    for location in locations
+      loc = new google.maps.LatLng(location[0], location[1])
+      marker = new google.maps.Marker({
+          map: map,
+          position: loc,
+      })
+      bounds.extend(loc)
+    map.setCenter(bounds.getCenter())
+    map.fitBounds(bounds)
+  else
+    map.setCenter(new google.maps.LatLng(39.303487,-76.609833))
+    map.setZoom(10)
 
 window.draw_listing_map = drawMap
--- spacefinder/static/js/main.js
+++ spacefinder/static/js/main.js
@@ -7,18 +7,23 @@
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     map = new google.maps.Map(document.getElementById('listing-map'), mapOptions);
-    bounds = new google.maps.LatLngBounds();
-    for (_i = 0, _len = locations.length; _i < _len; _i++) {
-      location = locations[_i];
-      loc = new google.maps.LatLng(location[0], location[1]);
-      marker = new google.maps.Marker({
-        map: map,
-        position: loc
-      });
-      bounds.extend(loc);
+    if (locations.length > 0) {
+      bounds = new google.maps.LatLngBounds();
+      for (_i = 0, _len = locations.length; _i < _len; _i++) {
+        location = locations[_i];
+        loc = new google.maps.LatLng(location[0], location[1]);
+        marker = new google.maps.Marker({
+          map: map,
+          position: loc
+        });
+        bounds.extend(loc);
+      }
+      map.setCenter(bounds.getCenter());
+      return map.fitBounds(bounds);
+    } else {
+      map.setCenter(new google.maps.LatLng(39.303487, -76.609833));
+      return map.setZoom(10);
     }
-    map.setCenter(bounds.getCenter());
-    return map.fitBounds(bounds);
   };
 
   window.draw_listing_map = drawMap;
