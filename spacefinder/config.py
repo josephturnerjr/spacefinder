223d04a0f57ea0f585529c6bc0393322626ac484
Joseph Turner <turner@miserware.com>
2013-10-22 22:04:30 -0400

    Make all the API keys config options

--- spacefinder/config.py
+++ spacefinder/config.py
@@ -1,5 +1,11 @@
 class Config(object):
-    SECRET_KEY = 'abc' 
+    SECRET_KEY = 'YOUR_SECRET_KEY'  # FILL IN YOUR SECRET KEY
+    # The domain configuration point is used both for Google Analytics
+    #   as well as for the 'from' address for outgoing emails
+    DOMAIN = 'example.com'  # Fill in the domain name this is hosted on
+    GOOGLE_ANALYTICS_API_KEY = 'YOUR_GA_API_KEY'  # Fill in your GA key
+    GOOGLE_MAPS_API_KEY = 'YOUR_GM_API_KEY'  # Fill in your GM key
+    MAILGUN_API_KEY = 'YOUR_MG_API_KEY'  # Fill in your mailgun key
 
 
 class ProductionConfig(Config):
--- spacefinder/sf_email.py
+++ spacefinder/sf_email.py
@@ -1,24 +1,30 @@
-import smtplib
-from email.mime.text import MIMEText
+from spacefinder import app
+import requests
 
-FROM_EMAIL = "support@example.com"
+FROM_EMAIL = "support@%s" % app.config['DOMAIN']
 
-TOKEN = {"subject": "Submit your nonprofit sublet",
-                "content": """Follow the link below to submit your sublet space:
+TOKEN = {
+    "subject": "Submit your nonprofit space",
+    "content": """Follow the link below to submit your space:
 
-http://0.0.0.0:5000/submission/%(token)s"""}
+http://%(domain)s/submission/%(token)s"""}
+
 
 def send_email(to_addr, subject, content, from_addr=FROM_EMAIL):
-    recipients = [to_addr]
-    msg = MIMEText(content)
-    msg["Subject"] = subject
-    msg['From'] = from_addr
-    msg['To'] = ", ".join(recipients)
-    c = smtplib.SMTP('localhost')
-    c.sendmail(from_addr, recipients, msg.as_string())
-    c.quit()
+    r = requests.post(
+        "https://api.mailgun.net/v2/samples.mailgun.org/messages",
+        auth=("api", app.config['MAILGUN_API_KEY']),
+        data={"from": "Excited User <me@samples.mailgun.org>",
+              "to": [to_addr],
+              "subject": subject,
+              "text": content})
+    print r.status_code
+    print r.text
+    return r
 
 
 def send_token(to_addr, submission_token):
-    return send_email(to_addr, TOKEN["subject"],
-                      TOKEN["content"] % {"token": submission_token})
+    return send_email(to_addr,
+                      TOKEN["subject"],
+                      TOKEN["content"] % {"token": submission_token,
+                                          "domain": app.config["DOMAIN"]})
