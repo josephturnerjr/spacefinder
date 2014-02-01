5dc298ae7e0afce086b192cb2294d5ca49ad96b9
Joseph Turner <turner@miserware.com>
2014-02-01 09:51:11 -0500

    Update the email and the disclaimer

--- spacefinder/sf_email.py
+++ spacefinder/sf_email.py
@@ -5,9 +5,20 @@ FROM_EMAIL = "Maryland Nonprofits <support@%s>" % app.config['DOMAIN']
 
 TOKEN = {
     "subject": "Submit your nonprofit space",
-    "content": """Follow the link below to submit your space:
+    "content": """Thanks for signing up to post your space on Maryland Nonprofits' Space Finder. To complete your submission, click on the link below.
 
-http://%(domain)s/submission/%(token)s"""}
+http://%(domain)s/submission/%(token)s
+
+If the link doesn't work, please copy and paste it into your browser window. Please submit listings for space only if the space is appropriate for use by a nonprofit organization. Please do not list residential spaces unless appropriately zoned for commercial use.
+
+Thanks again,
+Maryland Nonprofits
+1500 Union Ave, Suite 2500
+Baltimore, MD 21211
+(410) 727-6367 x2346
+Fax: (410) 235-2190
+http://www.marylandnonprofits.org
+"""}
 
 
 def send_email(to_addr, subject, content, from_addr=FROM_EMAIL):
